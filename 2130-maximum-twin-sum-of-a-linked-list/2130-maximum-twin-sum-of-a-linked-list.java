3a25e925808dfde556cfa9c743c233c4d1fd6478
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-06-15 00:10:51 +0530

    Time: 5 ms (58.5%), Space: 104.4 MB (27.33%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int pairSum(ListNode head) {
+        int size = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+        int[] arr = new int[size];
+        curr = head;
+        for (int i = 0; i < size; i++) {
+            arr[i] = curr.val;
+            curr = curr.next;
+        }
+        int maxSum = 0;
+        for (int i = 0; i < size / 2; i++) {
+            maxSum = Math.max(maxSum, arr[i] + arr[size - 1 - i]);
+        }
+        return maxSum;
+    }
+}
