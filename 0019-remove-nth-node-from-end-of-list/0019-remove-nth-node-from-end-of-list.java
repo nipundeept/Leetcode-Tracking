de0dd4d3bc189c528c749226f1e8bb14ba4826ae
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-06-24 13:25:54 +0530

    Time: 0 ms (100%), Space: 43.3 MB (76.04%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,19 +13,19 @@ class Solution {
         if (head == null || head.next == null) {
             return null;
         }
-        ListNode dummy = new ListNode (0);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        int size = 0;
-        ListNode curr = dummy;
-        while (curr != null) {
-            size++;
-            curr = curr.next;
+        ListNode slow = dummy; //initialising both slow and fast pointers
+        ListNode fast = dummy;
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next; // we move fast pointer in such as way that there is a n gap between slow and fast pointers
         }
-        curr = dummy;
-        for (int i = 0; i < size - n - 1; i++) {
-            curr = curr.next;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
         }
-        curr.next = curr.next.next;
+        //when fast is pointing to null, slow is pointing to 1 node behind the removal node
+        slow.next = slow.next.next;
         return dummy.next;
     }
 }
