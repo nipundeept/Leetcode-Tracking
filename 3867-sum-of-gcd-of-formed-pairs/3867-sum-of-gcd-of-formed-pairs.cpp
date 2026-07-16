844f90472cfbba91ce77d2c57e7696a198d046ea
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-16 11:35:37 +0530

    Time: 59 ms (96.62%), Space: 155.6 MB (75.58%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
-        vector<long long> prefixGcd;
-        int maxElem = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
+        int n = nums.size();
+        vector<int> prefixGcd(n);
+        int maxElem = 0; //since constraint mentions nums[i] is greater than or equal to 1
+        for (int i = 0; i < n; i++) {
             maxElem = max(maxElem, nums[i]);
-            prefixGcd.push_back(__gcd(nums[i], maxElem));
+            prefixGcd[i] = (__gcd(nums[i], maxElem));
         }
         sort(prefixGcd.begin(), prefixGcd.end());
         long long sum = 0; int left = 0, right = prefixGcd.size() - 1;
