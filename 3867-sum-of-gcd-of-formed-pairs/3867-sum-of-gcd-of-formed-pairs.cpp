7d893e46c991406ba9cadf8d804c45797eda2d91
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-16 11:30:27 +0530

    Time: 86 ms (15.98%), Space: 176.2 MB (8.91%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.cpp
@@ -5,21 +5,15 @@ public:
         int maxElem = INT_MIN;
         for (int i = 0; i < nums.size(); i++) {
             maxElem = max(maxElem, nums[i]);
-            prefixGcd.push_back(gcdCalculation(nums[i], maxElem));
+            prefixGcd.push_back(__gcd(nums[i], maxElem));
         }
         sort(prefixGcd.begin(), prefixGcd.end());
         long long sum = 0; int left = 0, right = prefixGcd.size() - 1;
         while (left < right) {
-            long long gcd = gcdCalculation(prefixGcd[left], prefixGcd[right]);
+            long long gcd = __gcd(prefixGcd[left], prefixGcd[right]);
             sum += gcd;
             left++; right--;
         }
         return sum;
-    }
-    long long gcdCalculation(long long a, long long b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcdCalculation(b, a % b);
     }
 };
