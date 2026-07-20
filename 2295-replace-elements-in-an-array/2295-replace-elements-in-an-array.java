44dfcf2139a13853c38b1399eb22c0019d123431
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-20 10:23:29 +0530

    Time: 38 ms (83.56%), Space: 143.3 MB (34.23%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -1,4 +1,22 @@
 class Solution {
+    /*
+    [1,2,4,6] and operations are [[1,3], [4,7], [3,8]]
+    initially storing [1,0],[2,1],[4,2],[6,3] in map (to get index of an element faster without searching it)
+    Now, iterating through operations array :
+    [1,3] => replace element 1 with 3
+    so get the index of element 1, viz 0
+    replace nums[0] with 3, the updated array becomes [3,2,4,6]
+    and update the map by removing that element 1 with newly added element in its replacement [3,0], therefore updated map becomes [3,0],[2,1],[4,2],[6,3]
+    --------------------------------------------------------------------------------------------------------
+    for the next query, we have [4,7]
+    find the index of 4 in nums (viz, 2) and replace nums[2] with 7 and update the map
+    new array : [3,2,7,6] and updated map becomes [3,0],[2,1],[7,2],[6,3]
+    --------------------------------------------------------------------------------------------------------
+     for the next query, we have [3,8]
+     find the index of 3 in nums (viz, 0) and replace nums[0] with 8 and update the map
+     new array : [8,2,7,6] and updated map becomes [8,0],[2,1],[7,2],[6,3]
+     and then return the final array
+    */
     public int[] arrayChange(int[] nums, int[][] operations) {
         Map<Integer, Integer> map = new HashMap<>(); //creating a map to store and update elements and their indices in array nums
         for (int i = 0; i < nums.length; i++) {
