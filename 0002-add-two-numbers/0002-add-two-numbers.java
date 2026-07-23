e68974d447db58cd8457b415abc02918364dde6a
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-23 11:33:43 +0530

    Time: 1 ms (100%), Space: 44.8 MB (99.93%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -14,6 +14,7 @@ class Solution {
         ListNode curr1 = l1, curr2 = l2;
         ListNode dummy = new ListNode(0);
         ListNode ansp = dummy;
+        //traversing both the list and there are chances that one of the list may point to null, so the loop terminates due to that list
         while (curr1 != null && curr2 != null) {
             sum = curr1.val + curr2.val + carry;
             ListNode node = new ListNode(sum % 10);
@@ -23,28 +24,30 @@ class Solution {
             curr1 = curr1.next;
             curr2 = curr2.next;
         }
+        //if list2 is empty, we traverse the list1 and continue the addition until it points to null
         while (curr1 != null) {
-            int data = curr1.val;
-            sum = data + carry;
+            sum = curr1.val + carry;
             ListNode node = new ListNode(sum % 10);
             carry = sum / 10;
             ansp.next = node;
             ansp = ansp.next;
             curr1 = curr1.next;
         }
+        //if list1 is empty, we traverse the list2 and continue the addition until it points to null
         while (curr2 != null) {
-            int data = curr2.val;
-            sum = data + carry;
+            sum = curr2.val + carry;
             ListNode node = new ListNode(sum % 10);
             carry = sum / 10;
-            ansp.next = node;
-            ansp = ansp.next;
+            ansp.next = node; //appending the current node to the chain of result node
+            ansp = ansp.next; //result node points to the latest added node
             curr2 = curr2.next;
         }
+        //if carry is remaining, we need to append that carry at the end, and if it does not exist, the result is already computed
         if (carry != 0) {
             ListNode node = new ListNode(carry);
             ansp.next = node;
         }
+        //returning the next node of the dummy node (which contains the resulting list)
         return dummy.next;
     }
 }
