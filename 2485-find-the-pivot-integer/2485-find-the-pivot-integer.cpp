4c015d0d923606ef29e987f3bcef34dfe2147162
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-24 23:15:57 +0530

    Time: 0 ms (100%), Space: 8 MB (71.53%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        for (int i = 1; i <= n; i++) {
-            if ((i * i) == ((n * n) + n) >> 1) {
-                return i;
-            }
-        }
-        return -1;
+        int total = n * (n + 1) / 2;
+        int x = sqrt(total);
+        return x * x == total ? x : -1;
     }
 };
