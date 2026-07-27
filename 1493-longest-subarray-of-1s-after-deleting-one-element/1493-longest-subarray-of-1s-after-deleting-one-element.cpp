7b847f9f72e27e081f8cefa32ef6b2b7a3dfd902
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-27 23:48:17 +0530

    Time: 2 ms (27.57%), Space: 60 MB (98.1%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        //a valid window can have at most 1 0s
+        int n = nums.size();
+        int left = 0, right, zeroCount = 0, res = -1;
+        for (right = 0; right < n; right++) {
+            if (nums[right] == 0) {
+                zeroCount++;
+            }
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            res = max(res, right - left);
+        }
+        return res;
+    }
+};
