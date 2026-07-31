b3cb0141bf3b7c99530ccfde9aa423d24b4e853f
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-07-31 16:28:10 +0530

    Time: 0 ms (100%), Space: 27.1 MB (45.84%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        bool flag = true;
         int k = 1, n = nums.size();
-        if (n == 1) {
+        if (n == 1)
             return false;
-        }
         for (int i = 0; i < n - 1; i++) {
             if ((k++) != nums[i]) {
-                flag = false;
-                break;
+                return false;
             }
         }
-        if (nums[n - 2] != nums[n - 1]) {
-            flag = false;
-        }
-        return flag;
+        return (nums[n - 2] == nums[n - 1]);
     }
 };
