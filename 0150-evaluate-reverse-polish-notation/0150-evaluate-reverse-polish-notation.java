841dc8d49bbc4d97272c1e8ff9d5b2ce34d633e7
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-08-08 15:08:16 +0530

    Time: 6 ms (87.21%), Space: 45.2 MB (78.93%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -2,65 +2,37 @@ class Solution {
     public int evalRPN(String[] tokens) {
         int n = tokens.length, val = 0;
         if (n == 0 || n == 1) {
-            return Integer.parseInt(tokens[n - 1]);
+            return Integer.valueOf(tokens[n - 1]);
         }
         Deque<Integer> st = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             String str = tokens[i];
             int a = 0, b = 0;
             if (str.equals("+")) {
-                if (!st.isEmpty()) {
-                a = st.peek();
-                st.pop();
-            }
-            if (!st.isEmpty()) {
-                b = st.peek();
-                st.pop();
-            }
-                val = (a + b);
-                st.push(val);
+                a = st.pop();
+                b = st.pop();
+                st.push(a + b);
             }
             else if (str.equals("-")) {
-                if (!st.isEmpty()) {
-                    a = st.peek();
-                    st.pop();
-                }
-                if (!st.isEmpty()) {
-                    b = st.peek();
-                    st.pop();
-                }
-                val = (b - a);
-                st.push(val);
+                a = st.pop();
+                b = st.pop();
+                st.push(b - a);
             }
             else if (str.equals("*")) {
-                if (!st.isEmpty()) {
-                    a = st.peek();
-                    st.pop();
-                }
-                if (!st.isEmpty()) {
-                    b = st.peek();
-                    st.pop();
-                }
-                val = (b * a);
-                st.push(val);
+                a = st.pop();
+                b = st.pop();
+                st.push(a * b);
             }
             else if (str.equals("/")) {
-                if (!st.isEmpty()) {
-                    a = st.peek();
-                    st.pop();
-                }
-                if (!st.isEmpty()) {
-                    b = st.peek();
-                    st.pop();
-                }
-                val = (b / a);
-                st.push(val);
+                a = st.pop();
+                b = st.pop();
+                st.push(b / a);
             }
             else {
-                st.push(Integer.parseInt(str));
+                st.push(Integer.valueOf(str));
             }
         }
-        return val;
+        return st.peek();
     }
 
 }
