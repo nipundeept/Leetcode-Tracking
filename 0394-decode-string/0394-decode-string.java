9aaf42cc73f289ffa8efc37a4c8d45a2129435a3
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-08-09 14:08:47 +0530

    Time: 0 ms (100%), Space: 42.8 MB (72.37%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -2,32 +2,32 @@ class Solution {
     public String decodeString(String s) {
         int n = s.length();
         Deque<Integer> num_stack = new ArrayDeque<>();
-        Deque<String> string_stack = new ArrayDeque<>();
-        int curr_num = 0; String curr_str = "";
+        Deque<StringBuilder> string_stack = new ArrayDeque<>();
+        int curr_num = 0; StringBuilder curr_str = new StringBuilder();
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
             if (Character.isDigit(ch)) {
                 curr_num = curr_num * 10 + (ch - '0');
             }
             else if (Character.isLowerCase(ch)) {
-                curr_str += ch;
+                curr_str.append(ch);
             }
             else if (ch == '[') {
                 num_stack.push(curr_num);
                 string_stack.push(curr_str);
                 curr_num = 0;
-                curr_str = "";
+                curr_str = new StringBuilder();
             }
             else {
-                String formed = "";
+                StringBuilder formed = new StringBuilder();
                 int t = num_stack.pop();
                 while (t > 0) {
-                    formed += curr_str;
+                    formed.append(curr_str);
                     t--;
                 }
-                curr_str = string_stack.pop() + formed;
+                curr_str = string_stack.pop().append(formed);
             }
         }
-        return curr_str;
+        return curr_str.toString();
     }
 }
