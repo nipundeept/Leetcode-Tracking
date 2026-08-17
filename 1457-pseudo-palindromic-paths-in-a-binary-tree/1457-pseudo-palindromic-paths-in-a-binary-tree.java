f4b1b03642e0ef7e263dd069643609b5d0de285f
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-08-18 01:47:21 +0530

    Time: 15 ms (42.89%), Space: 104.8 MB (56.13%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    public int pseudoPalindromicPaths (TreeNode root) {
+        int[] freq = new int[10];
+        helper(root, freq);
+        return count;
+    }
+
+    private void helper(TreeNode root, int[] freq) {
+        if(root == null) {
+            return;
+        }
+        freq[root.val]++;
+        if (root.left == null && root.right == null) {
+            if (palindromicPermutation(freq)) {
+                count++;
+                freq[root.val]--;
+                return;
+            }
+        }
+        helper(root.left, freq);
+        helper(root.right, freq);
+        freq[root.val]--;
+        return;
+    }
+    private boolean palindromicPermutation(int[] freq) { //freq is an array of frequency of the digits
+        int odd = 0, n = freq.length;
+        for (int i = 1; i < n; i++) {
+            if (freq[i] % 2 != 0) {
+                odd++;
+            }
+        }
+        if (odd == 0 || odd == 1) {
+            return true;
+        }
+        return false;
+    }
+}
