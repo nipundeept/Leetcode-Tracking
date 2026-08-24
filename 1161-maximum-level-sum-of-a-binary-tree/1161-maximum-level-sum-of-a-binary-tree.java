ad31c46878a345d0eb4f75fdf403ba5f801c95fe
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-08-24 11:36:39 +0530

    Time: 8 ms (96.3%), Space: 49.7 MB (5.68%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -14,7 +14,9 @@
  * }
  */
 class Solution {
+    /*
     public int maxLevelSum(TreeNode root) {
+        Depth First Search (DFS Approach)
         Map<Integer, Integer> map = new HashMap<>();
         int max = Integer.MIN_VALUE, result = 0;
         helper(root, 1, map);
@@ -36,5 +38,32 @@ class Solution {
         helper(root.right, depth + 1, map);
         map.put(depth, map.getOrDefault(depth, 0) + root.val);
         return;
+    }
+    */
+    //using BFS
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int result = -1, level = 1, maxSum = Integer.MIN_VALUE;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+                if (curr.left != null) {
+                    queue.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.offer(curr.right);
+                }
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                result = level;
+            }
+            level++;
+        }
+        return result;
     }
 }
