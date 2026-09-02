7b9707b1dc82664d49efff8fd0ebcb920adf2198
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-09-01 22:51:45 -0700

    Time: 0 ms (100%), Space: 43.4 MB (16.22%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> tree1 = new ArrayList<>();
+        List<Integer> tree2 = new ArrayList<>();
+        helper(root1, tree1);
+        helper(root2, tree2);
+        int size1 = tree1.size();
+        int size2 = tree2.size();
+        if (size1 != size2) {
+            return false;
+        }
+        for (int i = 0; i < size1; i++) {
+            if (!tree1.get(i).equals(tree2.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //bruteforce : creating a helper that would store the leaf values in a data structure for a tree and then we compare it
+    private void helper(TreeNode root, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left, path);
+        helper(root.right, path);
+        if (root.left == null && root.right == null) {
+            path.add(root.val);
+        }
+    }
+}
