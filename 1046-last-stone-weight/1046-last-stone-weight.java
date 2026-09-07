f3ee7e286bf08d08ad5e7330568e097949593414
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-09-07 10:20:10 -0700

    Time: 1 ms (99.7%), Space: 42.7 MB (84%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        //using maxHeap, we store all the elements of the array in the heap first, and pop out the first two, if the difference of those elements is non-zero, we offer it to the heap else we do not offer it and return the last standing element in the heap.
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+        while (!(maxHeap.size() == 0) && maxHeap.size() != 1) {
+            int a = maxHeap.poll();
+            int b = maxHeap.poll();
+            if ((a - b) != 0) {
+                maxHeap.offer(a - b);
+            }
+        }
+        if (maxHeap.size() == 0) {
+            return 0;
+        }
+        return maxHeap.peek();
+    }
+}
