52dc090ad6e47842e49591003f28c4b6089af42c
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-09-08 04:30:11 -0700

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        return (n >= 1000) ? n - 999 : 0;
+        if (n >= 1000) {
+            return n - 999;
+        }
+        return 0;
     }
 };
