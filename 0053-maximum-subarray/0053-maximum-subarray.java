d5f888b711be576b572e71c17d42eff66d8d4152
Nipun Deept <39118021+nipundeept@users.noreply.github.com>
2026-09-25 00:37:29 +0530

    Time: 1 ms (99.92%), Space: 77.5 MB (20.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //Kadane's algorithm : At every new index, we have two possibilities (i. To start afresh from the current element or append the current element with the previous best sum)
-        int max_sum = nums[0];
-        int curr_sum = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            curr_sum = Math.max(nums[i], curr_sum + nums[i]);
-            max_sum = Math.max(max_sum, curr_sum);
+        int n = nums.length;
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < n; i++) {
+            currSum = (currSum + nums[i] > nums[i]) ? currSum + nums[i] : nums[i];
+            maxSum = Math.max(currSum, maxSum);
         }
-        return max_sum;
+        return maxSum;
     }
 }
